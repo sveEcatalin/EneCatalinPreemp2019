2d9e65116f0f27ec216f7fcbd69d9b086a4c38ca
Ene Catalin <catalin.ene@softvision.ro>
2019-07-23 13:37:59 +0300

    made some changes

--- form-validation/javascript practice/event-handlers/exercises/myscript.js
+++ form-validation/javascript practice/event-handlers/exercises/myscript.js
@@ -1,6 +1,21 @@
 
 
 function displayImage(){
-    console.log("hello");
+    if(redPressed===false){
+        document.querySelector("img").classList.remove("hide");
+        document.querySelector("img").classList.add("show");   
+    }
+    else{
+        alert("NO IMAGE TO DISPLAY");
+    }
+}
 
+function hideImage(){
+    document.querySelector("img").classList.add("hide");  
+    document.querySelector("img").classList.remove("show");
+
+}
+
+function removeImage(){
+    document.querySelector("img").remove();
 }
