b2c5b8cb1e07854ab5a868b069fa4b32c9e1dfbc
Ene Catalin <catalin.ene@softvision.ro>
2019-07-23 16:38:08 +0300

    finished

--- form-validation/javascript practice/event-handlers/exercises/myscript.js
+++ form-validation/javascript practice/event-handlers/exercises/myscript.js
@@ -1,21 +1,65 @@
 
+let greenButton= document.querySelector(".btn-green");
+let redButton = document.querySelector(".btn-red");
+let blueButton=document.querySelector(".btn-blue");
+let imageParent= document.querySelector(".img-parent");
+let ostrichImage=document.querySelector(".ostrich-image");
+let bodyElem=document.querySelector("body");
 
-function displayImage(){
-    if(redPressed===false){
-        document.querySelector("img").classList.remove("hide");
-        document.querySelector("img").classList.add("show");   
-    }
-    else{
-        alert("NO IMAGE TO DISPLAY");
-    }
+
+
+greenButton.addEventListener("click",displayImage);
+ostrichImage.addEventListener("click",hideImage);
+ostrichImage.addEventListener("click",triggerParent)
+redButton.addEventListener("click",removeImage);
+// bodyElem. addEventListener("click", displayTarget,true);
+bodyElem. addEventListener("click", displayTarget);
+blueButton.addEventListener("click",triggerGreenAction);
+
+
+function displayImage(event){
+    
+    if (document.querySelector("img")==null){
+        alert("Image Deleted!");
+     }
+     else{
+        ostrichImage.classList.remove("hide");
+        ostrichImage.classList.add("show");
+     }
+     event.stopPropagation();
 }
 
-function hideImage(){
-    document.querySelector("img").classList.add("hide");  
-    document.querySelector("img").classList.remove("show");
+function hideImage(event){
+    ostrichImage.classList.add("hide");  
+    ostrichImage.classList.remove("show");
 
 }
 
-function removeImage(){
-    document.querySelector("img").remove();
+function removeImage(event){
+    ostrichImage.remove();
 }
+function displayTarget() {
+    console.log("Target is "+event.target + " CurrentTarget is ");
+    console.log(event.currentTarget);
+    // alert('Something was clicked');
+}
+
+function triggerGreenAction(){
+    greenButton.click();
+}
+
+function triggerParent(){
+    imageParent.dispatchEvent(customEvent);
+}
+
+
+let customEvent =new Event('customEvt');
+imageParent.addEventListener("customEvt",muhEvent,false);
+
+function muhEvent(){
+    imageParent.style.background="yellow";
+}
+
+
+// blueButton.click()
+//document.getElementsByClassName('img-parent')[0].click()
